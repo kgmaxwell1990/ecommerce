7ce75649066de8000a6687bcdb94be21dd80a761
Katie Maxwell <katie@codeinstitute.net>
2018-03-20 15:13:33 +0000

    added products app

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from home.views import *
-from accounts import urls as account_urls
 from django.views.static import serve
 from django.conf import settings
+from home.views import *
+from accounts import urls as account_urls
+from products import urls as product_urls
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', get_index, name="home"),
     url(r'^accounts/', include(account_urls)),
+    url(r'^products/', include(product_urls)),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
